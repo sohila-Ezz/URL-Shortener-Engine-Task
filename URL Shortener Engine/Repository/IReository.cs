e576d61060f0f9c6a4744be4a86c77e850ceb6e0
doaamahmoud <doaamahmoud048@gmail.com>
2022-06-12 01:15:50 +0200

    add interface(IRepository, IShort_UrlRepository )

---
+++ URL Shortener Engine/Repository/IReository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace URL_Shortener_Engine.Repository
+{
+    public interface IReository<T,T1>
+    {
+        Task<IEnumerable<T>> GetAll();
+        T Delete(T1 id);
+        Task<T> GetById(T1 id);
+        Task<T> Insert(T item);
+        T Update(T1 id, T item);
+
+    }
+}
---
+++ URL Shortener Engine/Repository/IShort_UrlRepository.cs
@@ -0,0 +1,8 @@
+﻿using URL_Shortener_Engine.Models;
+
+namespace URL_Shortener_Engine.Repository
+{
+    public interface IShort_UrlRepository:IReository<Short_Url,int>
+    {
+    }
+}
